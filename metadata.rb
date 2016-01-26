17d2de3014780ab67caaa7d7bfb797a8a33d8c1a
Donatas Abraitis <donatas@vinted.com>
2016-01-26 16:46:28 +0200

    Do not use IPv4 if not needed

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@dnsimple.com'
 license          'Apache 2.0'
 description      'Installs/Configures exabgp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.9'
+version          '0.1.10'
 
 supports 'ubuntu', '>= 12.04'
 
