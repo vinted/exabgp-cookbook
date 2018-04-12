b8c4513c5fa17476786e7d4920d20b8c94bfbde9
Dmitry Gritsay <dmitry@vinted.com>
2018-04-12 13:04:11 +0300

    Fix creation of systemd service
     - Bump cookbook version to `0.3.0`
     - Lock `systemd` version to `~> 3.2.2`
     - Move `description`, `after` and `condition_path_exists` under
       `unit` section

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'ops@dnsimple.com'
 license          'Apache 2.0'
 description      'Installs/Configures exabgp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
 
 supports 'ubuntu', '>= 12.04'
 supports 'centos', '>= 6.0'
 
 depends 'python'
 depends 'runit'
-depends 'systemd'
+depends 'systemd', '~> 3.2.2'
